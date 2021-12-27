1b32213a2701d9d0e76214e1e368c886c44642a2
Tilde Bastos <tilde.bastos@gmail.com>
2021-12-27 14:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Alura.ListaLeitura.App {
+    // métodos de iniciaização
+    public class Startup {
+        // adicionando o serviço de roteamento do Asp.Net Core
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app) {
+
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+     
+        }
+    }
+}
